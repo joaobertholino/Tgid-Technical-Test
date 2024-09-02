0bbed31e9f7809b0978300a6123205e3d6118603
João Bertholino <joaobertholino964@gmail.com>
2024-09-02 17:10:52 -0300

    feat: Implementation of exception template.

---
+++ src/main/java/dev/joaobertholino/tgidtechnicaltest/service/exception/exceptiontemplate/ExceptionTemplate.java
@@ -0,0 +1,62 @@
+package dev.joaobertholino.tgidtechnicaltest.service.exception.exceptiontemplate;
+
+import java.time.LocalDateTime;
+
+public class ExceptionTemplate {
+	private LocalDateTime timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+
+	public ExceptionTemplate() {
+	}
+
+	public ExceptionTemplate(LocalDateTime timestamp, Integer status, String error, String message, String path) {
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+}
