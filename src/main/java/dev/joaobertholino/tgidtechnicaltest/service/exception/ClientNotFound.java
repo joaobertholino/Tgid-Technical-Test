0f736287c4a2131a3301c9c3ea9fc08185bf2d8f
João Bertholino <joaobertholino964@gmail.com>
2024-09-02 17:10:07 -0300

    feat: Implementation of custom exceptions.

---
+++ src/main/java/dev/joaobertholino/tgidtechnicaltest/service/exception/ClientNotFound.java
@@ -0,0 +1,8 @@
+package dev.joaobertholino.tgidtechnicaltest.service.exception;
+
+public class ClientNotFound extends RuntimeException {
+	public ClientNotFound() {}
+	public ClientNotFound(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/dev/joaobertholino/tgidtechnicaltest/service/exception/EnterpriseNotFound.java
@@ -0,0 +1,8 @@
+package dev.joaobertholino.tgidtechnicaltest.service.exception;
+
+public class EnterpriseNotFound extends RuntimeException {
+	public EnterpriseNotFound() {}
+	public EnterpriseNotFound(String message) {
+		super(message);
+	}
+}
