e8eafb735a0458d3a504d984ba16898a4926fc75
João Bertholino <joaobertholino964@gmail.com>
2024-09-17 00:44:04 -0300

    Fix: Formatting

--- src/main/java/dev/joaobertholino/tgidtechnicaltest/TgidTechnicalTestApplication.java
+++ src/main/java/dev/joaobertholino/tgidtechnicaltest/TgidTechnicalTestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TgidTechnicalTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TgidTechnicalTestApplication.class, args);
 	}
-
 }
